f07d0dde8474d3648a772a4190aca281b8a6a31d
Godwill <kglikius@gmail.com>
2016-11-30 16:42:12 +0200

    Added some events

--- src/app/containers/notes-container/notes-container.component.ts
+++ src/app/containers/notes-container/notes-container.component.ts
@@ -5,9 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './notes-container.component.html',
   styleUrls: ['./notes-container.component.css']
 })
+
 export class NotesContainerComponent implements OnInit {
 
-  note = {title: 'This is a note', value: 'Eat some food'};
+  notes = [
+    {title: 'This is a note', value: 'Eat some food', color: 'lightblue'},
+    {title: 'This is a note', value: 'Read a book', color: 'green'},
+    {title: 'This is a note', value: 'Code some more', color: 'yellow'},
+    {title: 'This is a note', value: 'Check friends', color: 'red'},
+    {title: 'This is a note', value: 'Code some more', color: 'blue'},
+    {title: 'This is a note', value: 'Code some more', color: 'blue'}
+  ]
+
+  onNoteChecked(i) {
+    this.notes.splice(i, 1)
+  }
 
   constructor() { }
 
--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'note-card',
@@ -8,6 +8,19 @@ import {Component, OnInit, Input} from '@angular/core';
 export class NoteCardComponent implements OnInit {
 
   @Input() note = {};
+  @Output() checked = new EventEmitter();
+
+  onChecked() {
+    this.checked.next(this.note);
+  }
+
+  showCheck: boolean = false;
+
+  toggleCheck() {
+    this.showCheck = !this.showCheck;
+  }
+
+
   constructor() { }
 
   ngOnInit() {
